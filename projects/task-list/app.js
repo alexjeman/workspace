6adebe695dd57c74b3892ab9295d103e3bb273b9
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-15 16:54:27 +0200

    Filter tasks

--- projects/task-list/app.js
+++ projects/task-list/app.js
@@ -14,6 +14,8 @@ function loadEventListeners() {
   taskList.addEventListener("click", removeTask);
   // Clear tasks event
   clearBtn.addEventListener("click", clearTasks);
+  // Filter task event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add task
@@ -54,4 +56,18 @@ function clearTasks(e) {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
