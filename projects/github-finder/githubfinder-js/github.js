6d62bd48737b1e6ff612e168a3c5a16af0b5b49e
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-21 20:41:55 +0200

    Fetch github API/search user

---
+++ projects/github-finder/githubfinder-js/github.js
@@ -0,0 +1,16 @@
+class Github {
+  constructor() {
+    this.client_id = "b1331574c556dbb798d4";
+    this.client_secret = "e14aa76f05867902eaabb93d6f34359cd18881d4";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const profile = await profileResponse.json();
+    return {
+      profile: profile
+    };
+  }
+}
