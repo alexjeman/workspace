ee9f7c9446f0083f5f6328e4cbd038afa21818a9
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-24 01:52:05 +0200

    Draw stars on canvas, minor fixes

--- projects/starfield-simulation/app.js
+++ projects/starfield-simulation/app.js
@@ -3,16 +3,42 @@ let canvas = document.getElementById("canvas");
 // Set canvas size
 canvas.width = 400;
 canvas.height = 400;
-
 // Canvas context to 2D
-let c = canvas.getContext("2d");
+let context = canvas.getContext("2d");
+// Number of stars
+var numStars = 100;
+// Size of stars
+var size = 1;
+// Generate stars
+var stars = [];
+for (let i = 0; i < numStars; i++) {
+  stars[i] = new Star();
+}
+
+// Function to single star
+function Star() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  this.z = Math.random() * canvas.width;
+
+  this.show = function() {
+    context.beginPath();
+    context.fillStyle = "#ffffff";
+    context.arc(this.x, this.y, size, 0, 2 * Math.PI);
+    context.fill();
+  };
+}
 
 // Draw
 function draw() {
   // Canvas background
-  c.fillStyle = "#000000";
+  context.fillStyle = "#000000";
   // Draw a rectangle on canvas;
-  c.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < numStars; i++) {
+    stars[i].show();
+  }
 }
 
 // Update
@@ -20,5 +46,5 @@ function update() {
   draw();
   window.requestAnimationFrame(update);
 }
-
+console.log(stars);
 update();
