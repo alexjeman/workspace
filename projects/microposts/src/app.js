f7912907f4538e260fa5c0a0be665b66ff7076a4
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-25 23:48:39 +0200

    Add UI module, display microposts

--- projects/microposts/src/app.js
+++ projects/microposts/src/app.js
@@ -1,12 +1,13 @@
 import { http } from "./http";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import { ui } from "./ui";
 
 // Het post on DOM load
 document.addEventListener('DOMContentLoaded', getPosts)
 
 function getPosts() {
   http.get("http://localhost:3000/posts")
-    .then(data => console.log(data))
+    .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
