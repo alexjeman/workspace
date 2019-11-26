d73e4bc9fa1f4e1a3ccaaf72308e1d5702e3a5f8
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-26 12:52:33 +0200

    Html tag fix, webpack config indent

--- projects/microposts/webpack.config.js
+++ projects/microposts/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
- entry: {
-  app: ['./src/app.js']
- },
- output: {
-  path: path.resolve(__dirname, 'build'),
-  filename: 'app.bundle.js'
- },
- module: {
-  rules: [
-   {
-    test: /\.js?$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader',
-    query: {
-     presets: ['@babel/preset-env']
-    }
-   }
-  ]
- }
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  }
 };
