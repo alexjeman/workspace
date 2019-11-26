a5214c76b71925843f5769ae0f1815c1ff9497d6
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-11-26 13:19:36 +0200

    Delete micropost

--- projects/microposts/src/app.js
+++ projects/microposts/src/app.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", getPosts);
 // Listen for add post
 document.querySelector(".post-submit").addEventListener("click", submitPost);
 
+// Listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
 // Get posts
 function getPosts() {
   http
@@ -36,4 +39,19 @@ function submitPost() {
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+// Delete post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    http
+      .delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert("Post removed", "alert alert-success");
+        getPosts();
+      })
+      .catch(err => console.log(err));
+  }
 }
