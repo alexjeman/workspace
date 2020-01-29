a7a2a203998df1c5f85613fcafd49a7ae66aab00
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-29 18:48:26 +0200

    Creade model, register model in django admin

--- python/django_rest_yt2/REST_API/MODULES_API/models.py
+++ python/django_rest_yt2/REST_API/MODULES_API/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
